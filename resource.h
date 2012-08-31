34e7d7804ab3dcfa116c02f9c8d391b68a4b72e9
max_schmerz <max_schmerz@gmx.net>
2012-08-31 17:32:23 +0000

    + changed resources

--- resource.h
+++ resource.h
@@ -1,18 +1,59 @@
-#ifndef RESOURCE_H
-#define RESOURCE_H
+#ifndef RESOURCES_H
+#define RESOURCES_H
 
-struct RESOURCE
+/*******************************************************************
+*            INCLUDE THIS HEADER FILE INTO YOUR PROJECT            *
+********************************************************************
+* you can access binary data member:      RESOURCES::MEMBER        *
+* you can get the binary data size:       RESOURCES::MEMBER_SIZE   *
+* or you can get the size with sizeof:    sizeof RESOURCES::MEMBER *
+*******************************************************************/
+
+struct RESOURCES
 {
-    public: static const unsigned char _7Z_SFX[162816];
-    public: static const unsigned char _7ZS_SFX[140288];
-    public: static const unsigned char EXECUTE_FILE_PNG[361];
-    public: static const unsigned char EXECUTE_FILE_DISABLED_PNG[234];
-    public: static const unsigned char FOLDER_OPEN_PNG[379];
-    public: static const unsigned char FOLDER_OPEN_DISABLED_PNG[230];
-    public: static const unsigned char REPORT_PNG[152];
-    public: static const unsigned char REPORT_DISABLED_PNG[148];
-    public: static const unsigned char TITLE_PNG[155];
-    public: static const unsigned char TITLE_DISABLED_PNG[150];
+
+/*************************************************
+*   INCLUDE THIS SOURCE FILE INTO YOUR PROJECT   *
+*************************************************/
+	// File: "D:\- MY DATA -\Software-Projects\7z SFX Creator\trunk\res\7z.sfx"
+	public: static const unsigned char _7Z_SFX[162816];
+	public: static const unsigned int  _7Z_SFX_SIZE = 162816;
+
+	// File: "D:\- MY DATA -\Software-Projects\7z SFX Creator\trunk\res\7zS.sfx"
+	public: static const unsigned char _7ZS_SFX[140288];
+	public: static const unsigned int  _7ZS_SFX_SIZE = 140288;
+
+	// File: "D:\- MY DATA -\Software-Projects\7z SFX Creator\trunk\res\execute_file.png"
+	public: static const unsigned char EXECUTE_FILE_PNG[361];
+	public: static const unsigned int  EXECUTE_FILE_PNG_SIZE = 361;
+
+	// File: "D:\- MY DATA -\Software-Projects\7z SFX Creator\trunk\res\execute_file_disabled.png"
+	public: static const unsigned char EXECUTE_FILE_DISABLED_PNG[234];
+	public: static const unsigned int  EXECUTE_FILE_DISABLED_PNG_SIZE = 234;
+
+	// File: "D:\- MY DATA -\Software-Projects\7z SFX Creator\trunk\res\folder_open.png"
+	public: static const unsigned char FOLDER_OPEN_PNG[379];
+	public: static const unsigned int  FOLDER_OPEN_PNG_SIZE = 379;
+
+	// File: "D:\- MY DATA -\Software-Projects\7z SFX Creator\trunk\res\folder_open_disabled.png"
+	public: static const unsigned char FOLDER_OPEN_DISABLED_PNG[230];
+	public: static const unsigned int  FOLDER_OPEN_DISABLED_PNG_SIZE = 230;
+
+	// File: "D:\- MY DATA -\Software-Projects\7z SFX Creator\trunk\res\report.png"
+	public: static const unsigned char REPORT_PNG[152];
+	public: static const unsigned int  REPORT_PNG_SIZE = 152;
+
+	// File: "D:\- MY DATA -\Software-Projects\7z SFX Creator\trunk\res\report_disabled.png"
+	public: static const unsigned char REPORT_DISABLED_PNG[148];
+	public: static const unsigned int  REPORT_DISABLED_PNG_SIZE = 148;
+
+	// File: "D:\- MY DATA -\Software-Projects\7z SFX Creator\trunk\res\title.png"
+	public: static const unsigned char TITLE_PNG[155];
+	public: static const unsigned int  TITLE_PNG_SIZE = 155;
+
+	// File: "D:\- MY DATA -\Software-Projects\7z SFX Creator\trunk\res\title_disabled.png"
+	public: static const unsigned char TITLE_DISABLED_PNG[150];
+	public: static const unsigned int  TITLE_DISABLED_PNG_SIZE = 150;
 };
 
-#endif // RESOURCE_H
+#endif // RESOURCES_H
